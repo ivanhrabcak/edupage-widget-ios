501df44e1bfbd778f84538d66f5f573394eb5947
Ivan Hrabcak <ivan@hrabcak.eu>
2024-05-07 20:56:32 +0200

    Initial Commit

---
+++ EdupageWidget/EdupageWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdupageWidgetApp.swift
+//  EdupageWidget
+//
+//  Created by Ivan Hrabcak on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct EdupageWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
