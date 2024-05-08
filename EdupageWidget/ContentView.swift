a324f1e6c55174edde5dcdb5b36a13dea9b9bf9c
Charlie <meow@charliecat.space>
2024-05-09 01:06:21 +0200

    Add: add simple UI and an app icon

--- EdupageWidget/ContentView.swift
+++ EdupageWidget/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack (alignment: .leading, spacing: 5) {
+            Image(systemName: "graduationcap") // 􀫓
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text([
+                "",
+                "1.  Go to the Home Screen",
+                "2.  Go into the jiggle mode",
+                "3.  Use the + icon and look for Edupage Widget",
+                "4.  Add it",
+                "5.  Click on that widget and type in your Edupage login details",
+                ""
+            ].joined(separator: "\n"))
+            Link("Github", destination: URL(string: "https://github.com/ivanhrabcak/edupage-widget-ios")!)
         }
         .padding()
+        
     }
 }
 
